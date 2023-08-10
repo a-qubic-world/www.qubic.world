01025628c53b143dbc06991c715f43198be4902d
couch42 <couch42@qubic.world>
2023-08-11 01:50:30 +0200

    Fix warnings

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
-import { MeshStandardMaterial, Box3, Vector3 } from 'three'
+import { MeshStandardMaterial } from 'three'
 import { createRoot } from 'react-dom/client'
-import React, { useRef, useEffect, useState, Suspense } from 'react'
-import { Canvas, useFrame, ThreeElements, useLoader, useThree } from '@react-three/fiber'
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame, ThreeElements, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 // import App.css
@@ -21,7 +21,7 @@ function Box(props: ThreeElements['mesh']) {
       axis: "x",
       radiansRotated: 0
   })
-  const { camera, size } = useThree()
+  // const { camera, size } = useThree()
 
   const texture_1 = useLoader(TextureLoader, 'textures/1_dice.webp')
   const texture_2 = useLoader(TextureLoader, 'textures/2_dice.png')
@@ -37,14 +37,14 @@ function Box(props: ThreeElements['mesh']) {
     new MeshStandardMaterial({ map: texture_4 }),
     new MeshStandardMaterial({ map: texture_5 }),
     new MeshStandardMaterial({ map: texture_6 }),
-  ], []);
+  ], [texture_1, texture_2, texture_3, texture_4, texture_5, texture_6])
 
   useFrame(() => {
     if (ref.current) {
         let rotationIncrement = 0.01; // Change this to adjust speed
         let newRadiansRotated = rotationState.radiansRotated + rotationIncrement;
 
-        ref.current.rotation[rotationState.axis] += rotationIncrement;
+        ref.current.rotation[rotationState.axis] += rotationIncrement;        
 
         if (newRadiansRotated >= 2 * Math.PI) {
             setRotationState({
@@ -64,7 +64,7 @@ function Box(props: ThreeElements['mesh']) {
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      scale={hovered ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
